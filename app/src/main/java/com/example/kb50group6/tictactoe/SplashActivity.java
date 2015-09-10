88df80e9d8e08bc3a9fe1eee027822ef022093c0
RJTheil <rjtheil90@gmail.com>
2015-09-10 11:32:51 +0200

    Installed a Splash Screen/first try

--- app/src/main/java/com/example/kb50group6/tictactoe/SplashActivity.java
+++ app/src/main/java/com/example/kb50group6/tictactoe/SplashActivity.java
@@ -1,23 +1,48 @@
 package com.example.kb50group6.tictactoe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.os.Handler;
+
+/* Unused imports for now */
+import android.content.Context;
+import android.content.Intent;
+import android.os.Vibrator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 
 public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        houJeSmoel(1);
+
+        /* Create a new Runnable() which waits out the Splash Delay and opens up
+            the Main Activity */
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                // Close this activity
+                finish();
+                //onClickLogo();
+            }
+        }, SPLASH_TIME_OUT);
+
     }
-    public int nee = 5;
-    public void houJeSmoel(int ja)
-    {
-        ja = nee;
-    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
