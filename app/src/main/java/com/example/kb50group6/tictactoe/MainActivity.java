7bf0a96d8cf056c0b9423cff391299a9a399641c
RJTheil <rjtheil90@gmail.com>
2015-09-10 16:46:42 +0200

    Implemented an AlertDialog in method onClickButtonHowToPlay(); Here, two versions are implemented, of which the first is commented and not in use.

--- app/src/main/java/com/example/kb50group6/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/kb50group6/tictactoe/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.kb50group6.tictactoe;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,4 +40,57 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void onClickButtonPlay(View view){
+        finish();
+    }
+
+    public void onClickButtonHowToPlay(View view){
+
+        //TODO: decide which one of the following implementations we're going to use
+
+        /* We create a new DialogFragment of our "HowToPlayDialogFragment" Class -- */
+              //DialogFragment newFragment = new HowToPlayDialogFragment();
+        /* --and call show(). We give it a tag so we can do something with it afterwards (or simply because we have to, anyway): */
+              //newFragment.show(getSupportFragmentManager(), "howtoplay");
+
+        /*
+            The following code is another (better?) way of achieving the above. This way,
+            we don't need the "HowToPlayDialogFragment" anymore.
+         */
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this); //this Activity / This Context
+        builder.setMessage(R.string.how_to_play)
+                .setPositiveButton(R.string.OK, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Empty.
+                        // Here we decide what happens when the user clicks "Ok"
+                        Toast.makeText(getBaseContext(), "OK clicked!", Toast.LENGTH_SHORT).show();
+                    }
+                })
+        .show();
+
+    }
+
+    public void onClickButtonExit(View view){
+        finish();
+    }
+
+    public static class HowToPlayDialogFragment extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the Builder class for convenient dialog construction
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage(R.string.how_to_play)
+                    .setPositiveButton(R.string.OK, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // Empty.
+                            // Here we decide what happens when the user clicks "Ok"
+                        }
+                    });
+            // Create the AlertDialog object and return it
+            return builder.create();
+        }
+    }
+
 }
